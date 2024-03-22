07d83a1a2567d18ea91ec6fdc15c1dc2c3c452e7
Lakshit <rajputlakshit0@gmail.com>
2024-03-22 13:47:41 +0530

    Working functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-
-
-import React, { useState, useEffect } from 'react';
-
-
+import React, { useState, useRef } from 'react';
 
 function App() {
-  const [targetDate, setTargetDate] = useState('');
+  const [targetDate, setTargetDate] = useState("");
   const [countdown, setCountdown] = useState({
     days: 0,
     hours: 0,
     minutes: 0,
     seconds: 0
   });
+  const [completed, setCompleted] = useState(false);
+  const intervalRef = useRef<number>();
 
-  useEffect(() => {
-    const interval = setInterval(() => {
+  const startTimer = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+    intervalRef.current = window.setInterval(() => {
       const now = new Date().getTime();
       const distance = new Date(targetDate).getTime() - now;
 
-      if (distance < 0) {
-        clearInterval(interval);
+      if (distance <= 0) {
+        clearInterval(intervalRef.current);
+        setCompleted(true);
         setCountdown({
           days: 0,
           hours: 0,
@@ -31,7 +33,7 @@ function App() {
         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+        setCompleted(false);
         setCountdown({
           days,
           hours,
@@ -40,44 +42,51 @@ function App() {
         });
       }
     }, 1000);
+  }
 
-    return () => clearInterval(interval);
-  }, [targetDate]);
-
-  const handleInputChange = (event: any) => {
+  const handleInputChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
     setTargetDate(event.target.value);
   };
 
-  return (<>
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: '90lvh', flexDirection: "column" }}>
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: '90vh', flexDirection: "column" }}>
       <div>
-        <h1 style={{ color: 'white' }}>Countdown <span style={{ color: 'red', }}>Timer</span></h1>
+        <h1 style={{ color: 'white' }}>Countdown <span style={{ color: 'red' }}>Timer</span></h1>
       </div>
       <div style={{ textAlign: "center" }}>
         <input type="datetime-local" id="dateInput" value={targetDate} onChange={handleInputChange} />
-        <button>Start</button>
+        <button onClick={startTimer}>Start</button>
         <div className='countdownGroup'>
-          <div>
-            <p>{countdown.days}</p>
-            <p>Days</p>
-          </div>
-          <div>
-            <p>{countdown.hours}</p>
-            <p>Hours</p>
-          </div>
-          <div>
-            <p>{countdown.minutes}</p>
-            <p>Minutes</p>
-          </div>
-          <div>
-            <p>{countdown.seconds}</p>
-            <p>Seconds</p>
-          </div>
+          {completed ? (
+            <p style={{ color: 'purple', fontWeight: 600 }}>
+              🎉The countdown is over! What's next on your adventure?🎉
+            </p>
+          ) : countdown.days >= 100 ? (
+            <p style={{ color: 'purple', fontWeight: 600 }}>Selected time is more than 100 days</p>
+          ) : (
+            <>
+              <div>
+                <p>{countdown.days || 0}</p>
+                <p>Days</p>
+              </div>
+              <div>
+                <p>{countdown.hours || 0}</p>
+                <p>Hours</p>
+              </div>
+              <div>
+                <p>{countdown.minutes || 0}</p>
+                <p>Minutes</p>
+              </div>
+              <div>
+                <p>{countdown.seconds || 0}</p>
+                <p>Seconds</p>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
-  </>
-  )
+  );
 }
 
-export default App
+export default App;
